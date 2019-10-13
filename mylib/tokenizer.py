11475d42ae587325fff20d6b82c818354b3af77b
khj68 <43369570+khj68@users.noreply.github.com>
2019-10-13 17:11:17 +0900

    Update tokenizer.py

--- mylib/tokenizer.py
+++ mylib/tokenizer.py
@@ -1,13 +1,13 @@
-from nltk.stem.porter import PorterStemmer
-from nltk.corpus import stopwords
+# from nltk.stem.porter import PorterStemmer
+# from nltk.corpus import stopwords
 
-porter = PorterStemmer()
-stop = stopwords.words('english')
+# porter = PorterStemmer()
+# stop = stopwords.words('english')
 
 # torkenize by whitespace
 def tokenizer(text):
   return text.split()
 
-# torkenize by Porter Stemming Algorithm
-def tokenizer_porter(text):
-  return [porter.stem(word) for word in text.split()]
+# # torkenize by Porter Stemming Algorithm
+# def tokenizer_porter(text):
+#   return [porter.stem(word) for word in text.split()]
