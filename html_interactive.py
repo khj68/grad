3c9665c6ff526cf794a591838e6a3ed37d4feb50
khj68 <43369570+khj68@users.noreply.github.com>
2019-10-16 08:38:37 +0900

    Update html_interactive.py

--- html_interactive.py
+++ html_interactive.py
@@ -14,17 +14,17 @@ start_time = time()
 # name = form.getvalue('name')
 # comment = form.getvalue('textcontent')
 
-df = pd.read_csv('./data/refined_movie_review.csv')
+# df = pd.read_csv('./data/refined_movie_review.csv')
 
-X_train = df.loc[:35000, 'review'].values
-y_train = df.loc[:35000, 'sentiment'].values
-X_test = df.loc[15000:, 'review'].values
-y_test = df.loc[15000:, 'sentiment'].values
+# X_train = df.loc[:35000, 'review'].values
+# y_train = df.loc[:35000, 'sentiment'].values
+# X_test = df.loc[15000:, 'review'].values
+# y_test = df.loc[15000:, 'sentiment'].values
 
 curDir = os.getcwd()
 clf = pickle.load(open(os.path.join(curDir, 'data', 'pklObject', 'classifier.pkl'), 'rb'))
 
-y_pred = clf.predict(X_test)
+# y_pred = clf.predict(X_test)
 # print('precission of test : %.3f' %accuracy_score(y_test, y_pred))
 
 label = {0:'negative comment', 1:'positive comment'}
